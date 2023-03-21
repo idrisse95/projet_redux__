36fb2ca3284d4a6736788e577f4d2bb3e74ade91
55973 <55973@etu.he2b.be>
2023-03-21 11:33:44 +0100

    eiei

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.scss'
+import { Sidebar } from './components/idriss/Sidebar'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <Sidebar/>
+    </div>
+  )
+}
+
+export default App
